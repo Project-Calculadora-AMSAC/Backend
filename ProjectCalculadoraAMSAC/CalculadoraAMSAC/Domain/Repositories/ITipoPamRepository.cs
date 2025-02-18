7e00ac4f3e7ed57c74c30b34cf7bc2f111f325af
BrunoPa20 <u201914974@upc.edu.pe>
2025-02-17 22:26:12 -0500

    avance-TIPOPAM

---
+++ ProjectCalculadoraAMSAC/CalculadoraAMSAC/Domain/Repositories/ITipoPamRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProjectCalculadoraAMSAC.CalculadoraAMSAC.Domain.Model.Entities;
+using ProjectCalculadoraAMSAC.Shared.Domain.Repositories;
+
+namespace ProjectCalculadoraAMSAC.CalculadoraAMSAC.Domain.Repositories;
+
+public interface ITipoPamRepository : IBaseRepository<TipoPam>
+{
+    Task<TipoPam?> GetByIdAsync(int id);
+    Task<List<TipoPam>> GetAllAsync();
+}
---
+++ ProjectCalculadoraAMSAC/CalculadoraAMSAC/Infrastructure/Persistence/EFC/Repositories/TipoPamRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectCalculadoraAMSAC.CalculadoraAMSAC.Domain.Model.Entities;
+using ProjectCalculadoraAMSAC.CalculadoraAMSAC.Domain.Repositories;
+using ProjectCalculadoraAMSAC.Shared.Infraestructure.Persistences.EFC.Configuration;
+using ProjectCalculadoraAMSAC.Shared.Infraestructure.Persistences.EFC.Repositories;
+
+namespace ProjectCalculadoraAMSAC.CalculadoraAMSAC.Infrastructure.Persistence.EFC.Repositories;
+
+
+public class TipoPamRepository : BaseRepository<TipoPam>, ITipoPamRepository
+{
+    private readonly AppDbContext _context;
+
+    public TipoPamRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<TipoPam?> GetByIdAsync(int id)
+    {
+        return await _context.TipoPam
+            .Include(tp => tp.Atributos)
+            .Include(tp => tp.Variables)
+            .FirstOrDefaultAsync(tp => tp.Id == id);
+    }
+
+    public async Task<List<TipoPam>> GetAllAsync()
+    {
+        return await _context.TipoPam
+            .Include(tp => tp.Atributos)
+            .Include(tp => tp.Variables)
+            .ToListAsync();
+    }
+}
